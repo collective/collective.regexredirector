4c5921935f932bcdec9695d82463015d8c41331c
Julien Marinescu <davisp@xenbox.fr>
2013-02-01 10:21:11 +0100

    refactoring

--- collective/regexredirector/tests/base.py
+++ collective/regexredirector/tests/base.py
@@ -1,14 +1,5 @@
-import transaction
 import unittest2 as unittest
-from zope import interface
-from plone.app import testing
-from collective.regexredirector import testing
 from Products.PloneTestCase import PloneTestCase
-
-class UnitTestCase(unittest.TestCase):
-
-	def setUp(self):
-		pass
 
 class RedirectorTestCase(PloneTestCase.PloneTestCase):
 	pass
